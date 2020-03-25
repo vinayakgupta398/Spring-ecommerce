fbc70934cb00f1bb5f7b29dc9307a3cc0d108c71
unknown <vinayak.gupta16@gmail.com>
2020-03-25 19:40:27 -0400

    ecommerce-begin

---
+++ ecommerce-starter-pack/src/main/java/com/ecommerce/vinkishop/ecommercestarterpack/EcommerceStarterPackApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.vinkishop.ecommercestarterpack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceStarterPackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceStarterPackApplication.class, args);
+	}
+
+}
---
+++ ecommerce-starter-pack/src/test/java/com/ecommerce/vinkishop/ecommercestarterpack/EcommerceStarterPackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.vinkishop.ecommercestarterpack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceStarterPackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
